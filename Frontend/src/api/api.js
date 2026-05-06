69df0de42c0541446d440294230fbc3b491dfbd2
vivekanandkumawat262 <vivekanandkumawat262@gmail.com>
2026-05-06 17:53:57 +0530

    added backend url

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://ai-lead-generation-client-closing-engine-mqqd.onrender.com";
 
 export async function apiFetch(endpoint, options = {}) {
   const token = localStorage.getItem("token");
